6b53e446299ff3ee8208c663e23af6a86e5da9a2
jaca230 <jaca230@uky.edu>
2025-07-31 00:08:25 -0400

    first commit

---
+++ include/analysis_pipeline/wfd5/LinkDef.h
@@ -0,0 +1,10 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class dataProducts::WFD5TraceIntegral+;
+#pragma link C++ class WFD5WaveformsIntegratorStage+;
+#pragma link C++ class WFD5TraceIntegralHistogramStage+;
+
+#endif
