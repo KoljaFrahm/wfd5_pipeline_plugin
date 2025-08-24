89def07ebf47767e299f41ba2208b05523640cbf
Jack Carlton <115968567+jaca230@users.noreply.github.com>
2025-08-24 19:59:21 +0200

    Update wfd5_hodoscope_position_histogram_stage.cpp

--- src/analysis_pipeline/wfd5/stages/wfd5_hodoscope_position_histogram_stage.cpp
+++ src/analysis_pipeline/wfd5/stages/wfd5_hodoscope_position_histogram_stage.cpp
@@ -101,7 +101,7 @@ void WFD5HodoscopePositionHistogramStage::Process() {
 }
 
 void WFD5HodoscopePositionHistogramStage::FillHistogram(TH2D* hist, const HodoscopeEvent* evt) {
-    if (evt->max_integral_x > 0 || evt->max_integral_y < 0) {
+    if (evt->max_integral_x < 0 || evt->max_integral_y > 0) {
         spdlog::debug("[{}] Skipping fill: invalid max_integral_x={} or max_integral_y={}",
                       Name(), evt->max_integral_x, evt->max_integral_y);
         return;
